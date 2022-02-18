64cbd9a53e66f29e2d1a0be3a7de1160b0b35e30
zine el abidine <mahdizmen@hotmail.com>
2022-02-18 11:46:04 +0100

    modif

--- function.php
+++ function.php
@@ -1,19 +1,6 @@
 <?php
 
 
-function seConnecter() {
-    require_once __DIR__ . '/model/SimpleOrm.class.php';
-
-   
-    $conn = new mysqli('localhost', 'root', 'root');
-
-    if ($conn->connect_error)
-        die(sprintf('Unable to connect to the database. %s', $conn->connect_error));
-
-    
-    SimpleOrm::useConnection($conn, 'projet_php');
-}
-
 function redirection(string $route) {
     header('Location: ' . url($route));
     die;
