d295efbb23000c4513792978360383421e9472e9
zine el abidine <mahdizmen@hotmail.com>
2022-02-21 16:29:04 +0100

    modif ok

--- views/modif-article.php
+++ views/modif-article.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 
-require_once __DIR__ . '/header.php';?>
+require_once __DIR__ . '/header.php'; ?>
 <?php require_once __DIR__ . '/nav.php';
 
 ?>
@@ -8,50 +8,48 @@ require_once __DIR__ . '/header.php';?>
 <h1>modifier un article</h1>
 
 
-<form method="post">
+<form method="post" action="index.php?route=modif-article-handler&id=<?= $article->id ?>">
 
-
- 
 
     <div class="form-group row">
         <label for="titre" class="col-12 col-form-label">titre</label>
         <div class="col-12">
-            <input type="text" class="form-control" name="titre" id="titre" placeholder="titre" value="<?= $article->titre ?>"required autofocus>
+            <input type="text" class="form-control" name="titre" id="titre" placeholder="titre" value="<?= $article->titre ?>" required autofocus>
         </div>
     </div>
-   
+
 
     <div class="form-group row">
         <label for="auteur" class="col-12 col-form-label">auteur</label>
         <div class="col-12">
-        
-        
-        
-            <input  type="text" class="form-control" name="auteur" id="auteur" placeholder="auteur" value="<?= $article->auteur ?>" required>
+
+
+
+            <input type="text" class="form-control" name="auteur" id="auteur" placeholder="auteur" value="<?= $article->auteur ?>" required>
         </div>
     </div>
 
     <div class="form-group row">
         <label for="contenu" class="col-12 col-form-label">contenu</label>
         <div class="col-12">
-            <input type="text" class="form-control" name="contenu" id="contenu" placeholder="contenu" value="<?= $article->contenu ?>"required>
+            <input type="text" class="form-control" name="contenu" id="contenu" placeholder="contenu" value="<?= $article->contenu ?>" required>
         </div>
     </div>
 
     <div class="form-group row">
         <label for="contenu" class="col-12 col-form-label">image</label>
         <div class="col-12">
-            <input type="url" class="form-control" name="image" id="image" placeholder="image" value="<?= $article->image ?>"required>
+            <input type="url" class="form-control" name="image" id="image" placeholder="image" value="<?= $article->image ?>" required>
         </div>
     </div>
 
     <div class="form-group row">
-        <button href="<?= url('liste&id='.$a->id) ?>" type="submit" class="btn btn-primary">Envoyer</button>
-      
-    
+        <button href="<?= url('liste&id=' . $a->id) ?>" type="submit" class="btn btn-primary">Envoyer</button>
+
+
     </div>
 
 
 </form>
 <?php
-require_once __DIR__ . '/footer.php';?>
+require_once __DIR__ . '/footer.php'; ?>
