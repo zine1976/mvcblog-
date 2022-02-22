d95ede4f3466d674eb7c075575ea5fa439a431f0
zine el abidine <mahdizmen@hotmail.com>
2022-02-22 15:17:00 +0100

    modif

--- controller/modif-article-controller.php
+++ controller/modif-article-controller.php
@@ -15,6 +15,8 @@ function modifier()
 
 function modifier_handler()
 {
+    if (empty($_SESSION['role']) || $_SESSION['role'] != 'admin') die('Erreur 403');
+
     if (empty($_GET['id'])) erreur(404);
 
     require_once model('article');
